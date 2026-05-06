168f90bbb9abdfb93e8339fee9f8677367cdd6e0
Amirhossein Firoozi <amir@Amirhosseins-MacBook-Air.local>
2026-05-06 18:39:47 +0200

    Clean deploy from server sync 20260506_183712

--- storage/framework/views/05c671baf1491c3e951daf9ac98576c29ec50a2c.php
+++
@@ -1,6 +0,0 @@
-<button
-    <?php echo $button->getAttributes(); ?>
-
-    tabindex="0"
-><?php echo $button->label ?: $button->name; ?></button>
-<?php /**PATH /var/www/paymydine/app/admin/widgets/toolbar/button_button.blade.php ENDPATH**/ ?>
