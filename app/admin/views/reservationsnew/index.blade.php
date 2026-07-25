2caf8e892d1cf1649f2c98c977c985cf53191fa4
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-07-25 10:06:38 +0200

    Load Reservationsnew route layout authority

--- app/admin/views/reservationsnew/index.blade.php
+++ app/admin/views/reservationsnew/index.blade.php
@@ -5,4 +5,10 @@
     from a separate route. The old page remains untouched. Subsequent cleanup
     work will replace this include with isolated markup/assets on this route.
 --}}
+<script>
+(function () {
+    document.documentElement.classList.add('pmd-reservationsnew-route');
+})();
+</script>
+<link rel="stylesheet" href="/app/admin/assets/css/pmd-reservationsnew-layout-v1.css?v=20260725-1">
 @include('admin::reservations2.index')
