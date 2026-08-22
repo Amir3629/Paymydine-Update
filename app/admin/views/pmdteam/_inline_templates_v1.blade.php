39030b83c771cb129cb0c589730e4d97f9db1d09
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-22 12:51:03 +0200

    R43: pass fixed role and KDS station choices to staff form

--- app/admin/views/pmdteam/_inline_templates_v1.blade.php
+++ app/admin/views/pmdteam/_inline_templates_v1.blade.php
@@ -1,13 +1,24 @@
 @php
-    $roles = $pmdTeam['roles'] ?? collect();
+    $roleOptions = $pmdTeam['role_options'] ?? [];
+    $roleSelections = $pmdTeam['role_selections'] ?? [];
 @endphp
 <div id="pmd-team-inline-templates" data-pmd-inline-templates hidden>
     <template data-pmd-inline-template="team:staff:create">
-        @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'create','member'=>null,'roles'=>$roles])
+        @include('admin::pmdteam._inline_staff_form_v1', [
+            'mode' => 'create',
+            'member' => null,
+            'roleOptions' => $roleOptions,
+            'selectedRoleValue' => '',
+        ])
     </template>
     @foreach($staff as $member)
         <template data-pmd-inline-template="team:staff:edit:{{ $member->staff_id }}">
-            @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'edit','member'=>$member,'roles'=>$roles])
+            @include('admin::pmdteam._inline_staff_form_v1', [
+                'mode' => 'edit',
+                'member' => $member,
+                'roleOptions' => $roleOptions,
+                'selectedRoleValue' => $roleSelections[(int)$member->staff_id] ?? '',
+            ])
         </template>
     @endforeach
 </div>
