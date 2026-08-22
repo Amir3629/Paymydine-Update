c7902299d794a7dbb967fef7ab3e89ca2edee5cc
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-22 12:23:06 +0200

    R43 remove role editor templates from Team

--- app/admin/views/pmdteam/_inline_templates_v1.blade.php
+++ app/admin/views/pmdteam/_inline_templates_v1.blade.php
@@ -1,24 +1,13 @@
 @php
-    $groups = $pmdTeam['groups'] ?? collect();
-    $languages = $pmdTeam['languages'] ?? collect();
-    $permissions = $pmdTeam['permissions'] ?? [];
-    $canSuperUser = !empty($pmdTeam['can_super_user']);
+    $roles = $pmdTeam['roles'] ?? collect();
 @endphp
 <div id="pmd-team-inline-templates" data-pmd-inline-templates hidden>
     <template data-pmd-inline-template="team:staff:create">
-        @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'create','member'=>null,'groups'=>$groups,'roles'=>$roles,'languages'=>$languages,'canSuperUser'=>$canSuperUser])
+        @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'create','member'=>null,'roles'=>$roles])
     </template>
     @foreach($staff as $member)
         <template data-pmd-inline-template="team:staff:edit:{{ $member->staff_id }}">
-            @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'edit','member'=>$member,'groups'=>$groups,'roles'=>$roles,'languages'=>$languages,'canSuperUser'=>$canSuperUser])
-        </template>
-    @endforeach
-    <template data-pmd-inline-template="team:role:create">
-        @include('admin::pmdteam._inline_role_form_v1', ['mode'=>'create','role'=>null,'permissions'=>$permissions])
-    </template>
-    @foreach($roles as $role)
-        <template data-pmd-inline-template="team:role:edit:{{ $role->staff_role_id }}">
-            @include('admin::pmdteam._inline_role_form_v1', ['mode'=>'edit','role'=>$role,'permissions'=>$permissions])
+            @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'edit','member'=>$member,'roles'=>$roles])
         </template>
     @endforeach
 </div>
