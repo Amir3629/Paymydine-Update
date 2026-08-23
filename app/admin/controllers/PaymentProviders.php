c47494b3b9bf3d85f672be861b570d5590206a26
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-23 11:05:38 +0200

    payments: add provider catalogue endpoint

---
+++ app/admin/controllers/PaymentProviders.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Admin\Controllers;
+
+use Admin\Classes\AdminController;
+use App\Services\Payments\ProviderCapabilityRegistry;
+use App\Services\Payments\ProviderConnectionService;
+
+class PaymentProviders extends AdminController
+{
+    public function state(
+        ProviderCapabilityRegistry $registry,
+        ProviderConnectionService $connections
+    ) {
+        $state = $connections->state();
+        $connected = [];
+
+        foreach ($state['providers'] ?? [] as $provider) {
+            $connected[$provider['provider_code']] = $provider;
+        }
+
+        $providers = [];
+
+        foreach ($registry->definitions() as $code => $definition) {
+            $providers[] = array_merge([
+                'provider_code' => $code,
+                'connected' => isset($connected[$code]),
+                'environments' => [],
+            ], $definition, $connected[$code] ?? []);
+        }
+
+        return response()->json([
+            'ok' => true,
+            'schema_ready' => (bool)($state['schema_ready'] ?? false),
+            'providers' => $providers,
+        ]);
+    }
+}
