2a0f00dc5fb47500c3353be0f457a818a08cb99c
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-23 11:06:16 +0200

    payments: add provider settings page

---
+++ app/admin/views/paymentproviders/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin::_layouts.default')
+
+@section('content')
+<div class="container-fluid py-3">
+    <div data-pmd-payment-provider-catalogue>
+        <div class="text-muted">Loading payment providers...</div>
+    </div>
+</div>
+@endsection
