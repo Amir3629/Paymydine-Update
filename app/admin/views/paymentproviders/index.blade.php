d66ecb318ba4bb2b481365352377191c7472dcff
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-23 11:29:01 +0200

    payments: remove standalone provider page view

--- app/admin/views/paymentproviders/index.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="container-fluid py-3 pmd-provider-page">
-    <div data-pmd-payment-provider-catalogue>
-        <div class="text-muted">Loading payment providers...</div>
-    </div>
-</div>
