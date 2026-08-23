a1ecebbdd5a75499f313388e823be8ac996e773f
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-23 11:15:11 +0200

    payments: render provider page inside admin layout

--- app/admin/views/paymentproviders/index.blade.php
+++ app/admin/views/paymentproviders/index.blade.php
@@ -1,9 +1,5 @@
-@extends('admin::_layouts.default')
-
-@section('content')
-<div class="container-fluid py-3">
+<div class="container-fluid py-3 pmd-provider-page">
     <div data-pmd-payment-provider-catalogue>
         <div class="text-muted">Loading payment providers...</div>
     </div>
 </div>
-@endsection
