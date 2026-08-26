f966fde26c2560be9f3c97499bb18f3ebb89ecc5
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-26 19:02:31 +0200

    i18n: add browser consumer for platform catalogue

---
+++ app/admin/assets/js/pmd-platform-messages-v1.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+
+    if (window.PMDPlatformMessages) {
+        return;
+    }
+
+    var messages = window.PMD_PLATFORM_MESSAGES || {};
+    var locale = String(window.PMD_PLATFORM_LOCALE || 'en').toLowerCase();
+
+    function interpolate(value, replacements) {
+        var output = String(value == null ? '' : value);
+        var map = replacements && typeof replacements === 'object' ? replacements : {};
+
+        Object.keys(map).forEach(function (name) {
+            var replacement = String(map[name] == null ? '' : map[name]);
+            output = output
+                .split(':' + name).join(replacement)
+                .split('{' + name + '}').join(replacement);
+        });
+
+        return output;
+    }
+
+    function t(key, replacements, fallback) {
+        var value = Object.prototype.hasOwnProperty.call(messages, key)
+            ? messages[key]
+            : (fallback == null ? key : fallback);
+
+        return interpolate(value, replacements);
+    }
+
+    window.PMDPlatformMessages = Object.freeze({
+        version: '1.0.0',
+        locale: function () { return locale; },
+        has: function (key) {
+            return Object.prototype.hasOwnProperty.call(messages, key);
+        },
+        t: t,
+        inspect: function () {
+            return {
+                version: '1.0.0',
+                locale: locale,
+                keys: Object.keys(messages).length
+            };
+        }
+    });
+})();
