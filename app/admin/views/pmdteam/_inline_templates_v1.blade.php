be337b9afda00035d169a239fe53609936f0c763
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-28 21:45:14 +0200

    Expose restaurant people in Team inline editor (#185)

--- app/admin/views/pmdteam/_inline_templates_v1.blade.php
+++ app/admin/views/pmdteam/_inline_templates_v1.blade.php
@@ -1,7 +1,35 @@
 @php
     $roles = $pmdTeam['roles'] ?? collect();
+    $roster = collect($pmdTeam['roster'] ?? []);
+    $departments = $pmdTeam['departments'] ?? [];
+    $operationalRoles = $pmdTeam['operational_roles'] ?? [];
+    $staffOptions = collect($pmdTeam['staff_options'] ?? []);
 @endphp
 <div id="pmd-team-inline-templates" data-pmd-inline-templates hidden>
+    @if(!empty($pmdTeam['roster_ready']))
+        <template data-pmd-inline-template="team:person:create">
+            @include('admin::pmdteam._inline_roster_person_form_v1', [
+                'mode'=>'create',
+                'person'=>null,
+                'departments'=>$departments,
+                'operationalRoles'=>$operationalRoles,
+                'staffOptions'=>$staffOptions,
+            ])
+        </template>
+
+        @foreach($roster as $person)
+            <template data-pmd-inline-template="team:person:edit:{{ $person->id }}">
+                @include('admin::pmdteam._inline_roster_person_form_v1', [
+                    'mode'=>'edit',
+                    'person'=>$person,
+                    'departments'=>$departments,
+                    'operationalRoles'=>$operationalRoles,
+                    'staffOptions'=>$staffOptions,
+                ])
+            </template>
+        @endforeach
+    @endif
+
     <template data-pmd-inline-template="team:staff:create">
         @include('admin::pmdteam._inline_staff_form_v1', ['mode'=>'create','member'=>null,'roles'=>$roles])
     </template>
