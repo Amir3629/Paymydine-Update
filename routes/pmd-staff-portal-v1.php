3d08525e2672de1bf31477ac4b5a6ecb8fca22cf
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-08-29 16:52:58 +0200

    Add Staff Portal chat and management routes

--- routes/pmd-staff-portal-v1.php
+++ routes/pmd-staff-portal-v1.php
@@ -3,15 +3,16 @@
 use App\Http\Controllers\PmdStaffPortalController;
 use Illuminate\Support\Facades\Route;
 
-/** PMD_STAFF_PORTAL_V1 */
+/** PMD_STAFF_PORTAL_V2 */
 App::before(function () {
     Route::group(['middleware' => ['web']], function () {
         Route::get('/staff/login', [PmdStaffPortalController::class, 'login'])->name('pmd.staff.login');
-        Route::post('/staff/login', [PmdStaffPortalController::class, 'authenticate'])
-            ->middleware('throttle:8,15')
-            ->name('pmd.staff.authenticate');
+        Route::post('/staff/login', [PmdStaffPortalController::class, 'authenticate'])->middleware('throttle:8,15')->name('pmd.staff.authenticate');
         Route::get('/staff', [PmdStaffPortalController::class, 'index'])->name('pmd.staff.home');
-        Route::post('/staff/request', [PmdStaffPortalController::class, 'saveRequest'])->name('pmd.staff.request');
+        Route::post('/staff/request', [PmdStaffPortalController::class, 'saveRequest'])->middleware('throttle:30,1')->name('pmd.staff.request');
+        Route::post('/staff/request/handle', [PmdStaffPortalController::class, 'handleRequest'])->middleware('throttle:60,1')->name('pmd.staff.request.handle');
+        Route::post('/staff/groups', [PmdStaffPortalController::class, 'createGroup'])->middleware('throttle:20,1')->name('pmd.staff.groups.create');
+        Route::post('/staff/chat/message', [PmdStaffPortalController::class, 'sendChatMessage'])->middleware('throttle:120,1')->name('pmd.staff.chat.message');
         Route::post('/staff/logout', [PmdStaffPortalController::class, 'logout'])->name('pmd.staff.logout');
     });
 });
