26a9ed101271f9e659691f1110a28066880c3186
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-20 18:59:41 +0200

    Add Google Business callback and notification routes

---
+++ routes/google-business-profile.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\GoogleBusinessIntegrationController;
+use Illuminate\Support\Facades\Route;
+
+if (!defined('PMD_GOOGLE_BUSINESS_PROFILE_ROUTES_V2')) {
+    define('PMD_GOOGLE_BUSINESS_PROFILE_ROUTES_V2', true);
+
+    Route::get(
+        '/integrations/google-business/callback',
+        [GoogleBusinessIntegrationController::class, 'callback']
+    )->name('pmd.google-business.callback');
+
+    Route::post(
+        '/integrations/google-business/pubsub',
+        [GoogleBusinessIntegrationController::class, 'pubsub']
+    )->name('pmd.google-business.pubsub');
+}
