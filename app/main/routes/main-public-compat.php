27810610231ea95b122fb292e858c3e8e0d6f3b3
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-20 16:26:02 +0200

    Expose Google review URL in app/main/routes/main-public-compat.php

--- app/main/routes/main-public-compat.php
+++ app/main/routes/main-public-compat.php
@@ -50,6 +50,7 @@ Route::get('/api/v1/settings-wrapped', function () {
 
                 'pmd_social_google_enabled' => $get('pmd_social_google_enabled', '0'),
                 'pmd_social_google_url' => $get('pmd_social_google_url', ''),
+                'pmd_social_google_review_url' => $get('pmd_social_google_review_url', ''),
 
                 'pmd_social_trustpilot_enabled' => $get('pmd_social_trustpilot_enabled', '0'),
                 'pmd_social_trustpilot_url' => $get('pmd_social_trustpilot_url', ''),
