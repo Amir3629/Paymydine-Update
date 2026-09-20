5c7f02a951e3b9a31f104917a4f079c95e5f37eb
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-20 19:20:14 +0200

    Android: consume secure pairing callback intents

--- mobile/android/app/src/main/java/com/paymydine/mobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/paymydine/mobile/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.paymydine.mobile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,6 +10,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val app = application as PayMyDineApplication
+        app.handleIntent(intent)
         setContent { PayMyDineApp(app) }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        (application as PayMyDineApplication).handleIntent(intent)
     }
 }
