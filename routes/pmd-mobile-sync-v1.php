e9a629e65e4fafb9a89dadf8d420351a64663ca1
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-20 19:18:04 +0200

    Mobile pairing: wire browser and exchange routes

--- routes/pmd-mobile-sync-v1.php
+++ routes/pmd-mobile-sync-v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PmdMobileBootstrapController;
+use App\Http\Controllers\PmdMobilePairController;
 use App\Http\Controllers\PmdMobileSyncController;
 use Illuminate\Support\Facades\Route;
 use Igniter\Flame\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -14,10 +15,21 @@ use Igniter\Flame\Foundation\Http\Middleware\VerifyCsrfToken;
 App::before(function () {
     Route::group([
         'middleware' => ['web'],
+        'prefix' => config('system.adminUri', 'admin'),
+    ], function () {
+        Route::get('mobile/pair/start', [PmdMobilePairController::class, 'start']);
+        Route::get('mobile/pair/finish', [PmdMobilePairController::class, 'finish']);
+    });
+
+    Route::group([
+        'middleware' => ['web'],
         'prefix' => config('system.adminUri', 'admin').'/api/mobile/v1',
     ], function () {
         Route::get('bootstrap', PmdMobileBootstrapController::class);
         Route::get('sync/events', [PmdMobileSyncController::class, 'events']);
+
+        Route::post('pair/exchange', [PmdMobilePairController::class, 'exchange'])
+            ->withoutMiddleware([VerifyCsrfToken::class]);
 
         Route::post('sync/commands', [PmdMobileSyncController::class, 'commands'])
             ->withoutMiddleware([VerifyCsrfToken::class]);
