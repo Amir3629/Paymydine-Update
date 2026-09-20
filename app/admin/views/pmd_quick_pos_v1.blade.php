22a4ec8e7da2a2261ec35dbc34e6e70c230d9eda
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-20 18:57:25 +0200

    chore(pos): bump floating product body stylesheet

--- app/admin/views/pmd_quick_pos_v1.blade.php
+++ app/admin/views/pmd_quick_pos_v1.blade.php
@@ -7,7 +7,7 @@
     <meta name="theme-color" content="#064e3b">
     <title>PayMyDine POS</title>
     <link rel="icon" type="image/svg+xml" href="/app/admin/assets/images/pmd-favicon-final-20260822.svg">
-    <link rel="stylesheet" href="/app/admin/assets/css/pmd-quick-pos-v1.css?v=20260920-31">
+    <link rel="stylesheet" href="/app/admin/assets/css/pmd-quick-pos-v1.css?v=20260920-32">
 </head>
 <body class="pmd-qpos-body">
 @php
