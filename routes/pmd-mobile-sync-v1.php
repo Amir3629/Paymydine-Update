cbb446263cd459dec06568cab9295ebc0cab0bf5
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-21 20:16:40 +0200

    Mobile pairing: remove browser self-approval route

--- routes/pmd-mobile-sync-v1.php
+++ routes/pmd-mobile-sync-v1.php
@@ -30,7 +30,6 @@ Route::group([
     'prefix' => config('system.adminUri', 'admin'),
 ], function () {
         Route::get('mobile/pair/start', [PmdMobilePairController::class, 'start']);
-        Route::post('mobile/pair/approve', [PmdMobilePairController::class, 'approve']);
         Route::get('mobile/pair/wait', [PmdMobilePairController::class, 'wait']);
         Route::get('mobile/pair/finish', [PmdMobilePairController::class, 'finish']);
     Route::get('mobile/pos/open', PmdMobilePosSessionController::class)
