0f607ba9d142d3b7856f005284089393d0b2f954
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-21 16:30:33 +0200

    Settings: point Android POS to 0.2.4 landscape fix preview

--- app/admin/views/pmdsettings/index.blade.php
+++ app/admin/views/pmdsettings/index.blade.php
@@ -1185,7 +1185,7 @@ document.documentElement.classList.add(
             <a
                 class="pmd-cashier-launcher-v107__download"
                 {{-- PMD_ANDROID_POS_PREVIEW_V2_LINK --}}
-                href="https://github.com/Amir3629/Paymydine-Update/releases/download/pmd-android-local-first-preview/PayMyDine-POS-Tablet-Preview.apk"
+                href="https://github.com/Amir3629/Paymydine-Update/releases/download/pmd-android-local-first-preview/PayMyDine-POS-Tablet-Preview-0.2.4.apk"
                 target="_blank"
                 rel="noopener noreferrer"
             >
@@ -1200,7 +1200,7 @@ document.documentElement.classList.add(
                 </span>
                 <span class="pmd-cashier-launcher-v107__copy">
                     <strong>Android Tablet / POS</strong>
-                    <small>Android 8+ · POS Preview 0.2.3</small>
+                    <small>Android 8+ · POS Preview 0.2.4</small>
                 </span>
             </a>
 
