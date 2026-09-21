33a1c96f9801555502a62ce90fed491c16aa9541
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-21 14:47:56 +0200

    Settings: point Android POS button to install-safe preview package

--- app/admin/views/pmdsettings/index.blade.php
+++ app/admin/views/pmdsettings/index.blade.php
@@ -1184,7 +1184,8 @@ document.documentElement.classList.add(
         >
             <a
                 class="pmd-cashier-launcher-v107__download"
-                href="https://github.com/Amir3629/Paymydine-Update/releases/download/pmd-android-local-first-preview/PayMyDine-Android-Local-First-Preview.apk"
+                {{-- PMD_ANDROID_POS_PREVIEW_V2_LINK --}}
+                href="https://github.com/Amir3629/Paymydine-Update/releases/download/pmd-android-local-first-preview/PayMyDine-POS-Tablet-Preview.apk"
                 target="_blank"
                 rel="noopener noreferrer"
             >
@@ -1199,7 +1200,7 @@ document.documentElement.classList.add(
                 </span>
                 <span class="pmd-cashier-launcher-v107__copy">
                     <strong>Android Tablet / POS</strong>
-                    <small>Android 8+ · Download preview APK</small>
+                    <small>Android 8+ · POS Preview 0.2.3</small>
                 </span>
             </a>
 
