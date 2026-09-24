22caecf1d74ce73d41c2b86b14f266cdc5a52194
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-25 01:09:55 +0200

    Android 0.3.32 V107: publish authoritative Local-First release

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
         // PMD_ANDROID_0_3_32_V107_RELEASE_FINAL
         // PMD_ANDROID_0_3_32_V107_RELEASE_FINAL_R2
         // PMD_ANDROID_0_3_32_V107_RELEASE_FINAL_R3
+        // PMD_ANDROID_0_3_32_V107_RELEASE_FINAL_R4
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SYNC_PROTOCOL_VERSION", "\"pmd-sync-v1\"")
     }
