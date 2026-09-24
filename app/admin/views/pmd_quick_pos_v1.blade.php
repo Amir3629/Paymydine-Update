9292ad6eab8373c896b66fe2bd58047eb3c97a7a
Amir3629 <amirhossein.firoozi@hotmail.com>
2026-09-25 01:11:21 +0200

    V107: scope Local-First sync chip to native Android POS

--- app/admin/views/pmd_quick_pos_v1.blade.php
+++ app/admin/views/pmd_quick_pos_v1.blade.php
@@ -296,7 +296,9 @@
                 </label>
 
                 <div class="pmd-qpos-work-meta">
-                    <span class="pmd-qpos-sync-state-v104" data-qpos-sync-state>Checking sync…</span>
+                    @if($pmdAndroidPosV107)
+                        <span class="pmd-qpos-sync-state-v104" data-qpos-sync-state>Checking sync…</span>
+                    @endif
                     <time class="pmd-qpos-clock" data-qpos-clock>{{ now()->format('H:i') }}</time>
                 </div>
             </div>
